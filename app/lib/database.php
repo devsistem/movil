af3c9ad2ba3fb17593554b5c931e798135a2d4c2
pol <sistemas@linkear.net>
2016-12-21 13:10:39 -0500

    Update database.php

--- app/lib/database.php
+++ app/lib/database.php
@@ -7,7 +7,7 @@ class Database
 {
     public static function StartUp()
     {
-       $pdo = new PDO('mysql:host=localhost;dbname=bd_liquida;charset=utf8', 'userapkdb', 'Elbrayan/2017');
+       $pdo = new PDO('mysql:host=107.180.21.237;dbname=bd_liquida;charset=utf8', 'userapkdb', 'Elbrayan/2017');
         
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
