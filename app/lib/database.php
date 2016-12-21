09d081f80910a3e86bded025d1dd672e3d9396b6
pol <sistemas@linkear.net>
2016-12-21 13:09:10 -0500

    Update database.php

--- app/lib/database.php
+++ app/lib/database.php
@@ -7,7 +7,7 @@ class Database
 {
     public static function StartUp()
     {
-        $pdo = new PDO('mysql:host=127.0.0.1;dbname=mobileDB;charset=utf8', 'root', 'sistemas1.2');
+       $pdo = new PDO('mysql:host=localhost;dbname=bd_liquida;charset=utf8', 'userapkdb', 'Elbrayan/2017');
         
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
